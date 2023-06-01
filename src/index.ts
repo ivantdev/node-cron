358338b6d9dcc8ca8b8f8b1a7b005ad1c4be6608
Ivan Castellanos <ivan@ivant.dev>
2023-06-01 15:54:36 -0500

    added time in logs

--- src/index.ts
+++ src/index.ts
@@ -16,16 +16,17 @@ cron.schedule(fetchInterval, async () => {
     try {
       const response = await node_fetch(process.env.API_URL as string);
       status = response.status;
-      console.log(`status: ${status}`);
     } catch (error) {
       console.log(`error: ${error}`);
       max_attempts = max_attempts - 1;
     }
   }
   
+  const time = new Date().toISOString().slice(0, 23).replace('T', ' ');
   if (max_attempts === 0 && status !== 200) {
-    console.log(`max attempts reached, please check your API on ${process.env.API_URL}`);
+    console.log(`${time}: max attempts reached, please check your API on ${process.env.API_URL}`);
   } else {
-    console.log(`status ${status}: ${process.env.API_URL} - task completed with ${max_attempts} attempts left`);
+    // time in this format [2023-06-01 12:00:00.661]
+    console.log(`${time}: status ${status}: ${process.env.API_URL?.slice(0, 30)}... - task completed with ${max_attempts} attempts left`);
   }
 });
